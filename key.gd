94ecd157f6af5c3008f01a392864d183c3a68dcc
Frejbo <frej.hellsten@gmail.com>
2023-10-17 13:14:54 +0200

    Ändrat hur nyckeln plockas

--- key.gd
+++ key.gd
@@ -4,11 +4,14 @@ extends CharacterBody2D
 var goal_pos : Vector2
 @onready var MAX_SPEED = target.MAX_SPEED
 
-@export var DRAG_DISTANCE = 300
+@export var DRAG_DISTANCE : int= 300
+@export var COLLECT_DISTANCE : int = 50
+@export var Y_OFFSET : int = -75
+
 var collected = false
 func _physics_process(_delta):
 	goal_pos.x = target.position.x
-	goal_pos.y = target.position.y -75
+	goal_pos.y = target.position.y + Y_OFFSET
 	
 	var influence = 1-(position.distance_to(goal_pos) / DRAG_DISTANCE)
 	influence = clamp(influence, 0, 1)
@@ -19,7 +22,7 @@ func _physics_process(_delta):
 		velocity = ((goal_pos - position)) * (influence*(4*influence))
 		move_and_slide()
 		
-		if influence >= .9:
+		if position.distance_to(Vector2(target.position.x, target.position.y+Y_OFFSET)) <= COLLECT_DISTANCE:
 			$AnimatedSprite2D/collect.play("collect")
 			print("Collected key")
 			collected = true
